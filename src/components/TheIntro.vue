334826d1e25f887c826bb0b9690a2582210e8d6d
Ryan Burney <hello@ryanburney.com>
2019-05-21 22:41:45 -0600

    Init

---
+++ src/components/TheIntro.vue
@@ -0,0 +1,28 @@
+<!--
+	The Intro
+
+	Master heading and intro paragraph for the entire app.
+-->
+<template>
+	<header>
+		<TheLogo />
+		<h1>Weather Forecast</h1>
+		<p
+			class="intro-p"
+		>Some language about the app goes here...</p>
+	</header>
+</template>
+
+<script>
+    import TheLogo from './TheLogo.vue';
+
+	export default {
+        components: {
+            TheLogo,
+        }
+    };
+</script>
+
+<style lang="scss" scoped>
+	
+</style>
