48bedfed56a8b4699c86c80c284b35e09ba94335
Ryan Burney <hello@ryanburney.com>
2019-05-22 10:20:24 -0600

    Loop through weather results, fetching one for each day

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,25 @@
 <template>
 	<div>
 		<TheIntro />
-		<div v-if="responseError">
-			<h3>Eek! Something Went Wrong...</h3>
-			<div v-html="responseError"></div>
+		<div v-if="response">
+			<h3>Response</h3>
+			<h5>City: {{ response.city }}</h5>
+			<pre>{{ response.list }}</pre>
+			/////////
+			<pre>{{ response }}</pre>
+			=============================
+			=============================
+			=============================
+			<div v-show="everyEighthResult(index)" v-for="(item, index) in response.list" :key="index">
+				<h4>DT Text: {{ item.dt_txt }}</h4>
+				<h4>Main: {{ item.main }}</h4>
+				<pre>{{ item }}</pre>
+			</div>
+			
 		</div>
 		<div v-else>
-			<h3>Response</h3>
-			{{ response }}
+			<h3>Eek! Something Went Wrong...</h3>
+			<div v-html="responseError"></div>
 		</div>
 	</div>
 </template>
@@ -31,10 +43,15 @@
 			}
 		},
 		methods: {
+			// API returns 40 total times across 5 days; we are only interested in one time per day, so return each 8th time
+			everyEighthResult(i) {
+				return i % 8 === 0;
+			},
+
 			loadWeatherData() {
 				let self = this;
 				axios
-					// .get('https://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=' + self.apiKey)
+					// .get('https://api.openweathermap.org/data/2.5/forecast?id=6542285&units=imperial&APPID=' + self.apiKey)
 					.get('http://localhost:8080/weather-data.json')
 					.then(function(response) {
 						self.response = response.data;
