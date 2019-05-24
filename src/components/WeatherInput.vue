0b83f91a702ac2d5db906398c8b37f86d7c4f2c9
Ryan Burney <hello@ryanburney.com>
2019-05-24 13:37:57 -0600

    Create WeatherInput.vue

---
+++ src/components/WeatherInput.vue
@@ -0,0 +1,32 @@
+<template>
+	<span>
+		<input type="text" placeholder="any city in the world" :value="location" @input="lookupWeather">
+		You entered: {{ location }}
+	</span>
+</template>
+
+<script>
+	var _ = require("lodash");
+
+	export default {
+		data() {
+			return {
+				location: ""
+			};
+        },
+        
+		methods: {
+			lookupWeather: _.debounce(function(e) {
+				this.location = e.target.value;
+			}, 500)
+		}
+	};
+</script>
+
+<style lang="scss" scoped>
+	input {
+		border: 0;
+		border-bottom: 2px solid #ccc;
+		font-size: 48px;
+	}
+</style>
