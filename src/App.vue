abf86712634f25019f5fe6bfc60d53147c91a67a
Ryan Burney <hello@ryanburney.com>
2019-05-24 15:39:57 -0600

    Migrate input to app component, update styling

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,43 @@
 <template>
 	<div class="wrapper">
-		<!-- <TheIntro /> -->
-		<TheIntroInput @forecast="forecastData = $event" @forecastError="forecastDataError = $event" />
-		<div v-if="forecastData">
-			<!-- <pre>{{ response }}</pre> -->
+		<TheLogo />
+		<header class="intro">
+			<h1>Going Somewhere?</h1>
+			<p>Get the current and five-day forecast for <ApiRequestInput @forecast="forecastData = $event" @forecastError="forecastDataError = $event" /> so you can, as the Boy Scouts say, <strong>be prepared.</strong></p>
+		</header>
+		<div v-if="forecastDataError">
+			<BaseNotification header="Sorry Charlie" content="Looks like that place only exists in your imagination. Try again!"/>
+		</div>
+		<div v-else-if="forecastData">
+			<h2 class="forecast-header">5-Day Forecast for {{ forecastData.city.name }}, {{ forecastData.city.country }}</h2>
 			<div class="card-wrap">
 				<WeatherCard
 					v-for="(dailyForecast, index) in filteredForecastData"
 					v-bind="dailyForecast"
-					:city=forecastData.city
 					:key="index"
 				/>
 			</div>
-		</div>
-		<div v-else-if="forecastDataError">
-			<h3>Eek! Something Went Wrong...</h3>
-			<div v-html="forecastDataError"></div>
+			<p class="credit">
+				Weather data courtesy of <a href="https://openweathermap.org/">Open Weather Map</a>
+			</p>
 		</div>
 		<div v-else>
-			<h3>Waiting for you to enter something...</h3>
+			<BaseNotification header="Start Typing" content="Waiting for you to enter something..."/>
 		</div>
 	</div>
 </template>
 
 <script>
-	// import TheIntro from './components/TheIntro.vue';
-	import TheIntroInput from './components/TheIntroInput.vue';
+	import ApiRequestInput from './components/ApiRequestInput.vue';
+	import BaseNotification from './components/BaseNotification.vue';
+	import TheLogo from './components/TheLogo.vue';
 	import WeatherCard from './components/WeatherCard.vue';
 
 	export default {
 		components: {
-			// TheIntro,
-			TheIntroInput,
+			BaseNotification,
+			ApiRequestInput,
+			TheLogo,
 			WeatherCard
 		},
 
@@ -53,6 +59,45 @@
 
 <style lang="scss">
 	@import "scss/base.scss";
+
+	h2 {
+		color: $secondary-text;
+	}
+
+	h2.forecast-header {
+		border-top: 4px dotted #F9CFD7;
+		color: $secondary-text;;
+		padding-top: 1em;
+		padding-bottom: 1em;
+	}
+
+	.intro {
+		margin-bottom: 48px;
+		
+		h1 {
+			background: $secondary-text;;
+			border-radius: 8px;
+			color: white;
+			display: inline-block;
+			font-size: 48px;
+			margin-bottom: 0;
+			margin-left: -8px;
+			padding: 8px;
+		}
+	
+		p {
+			font-size: 48px;
+			line-height: 1.2;
+			margin-top: 24px;
+		}
+	}
+
+	.credit {
+		color: #b4b4b6;
+		font-size: 12px;
+		margin-top: 32px;
+    	text-align: right;
+	}
 
 	// TODO: move these to scoped component files
 	.wrapper {
