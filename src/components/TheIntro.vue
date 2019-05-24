0f267ca13a2e1d79728bb4d4069132cbf1bc8bff
Ryan Burney <hello@ryanburney.com>
2019-05-24 13:37:51 -0600

    Update intro language

--- src/components/TheIntro.vue
+++ src/components/TheIntro.vue
@@ -6,23 +6,38 @@
 <template>
 	<header>
 		<TheLogo />
-		<h1>Weather Forecast</h1>
-		<p
-			class="intro-p"
-		>Some language about the app goes here...</p>
+		<h1>Going Somewhere?</h1>
+		<div>
+			<p>Get the current and five-day forecast for <WeatherInput/> so you can, as the Boy Scouts say, <strong>be prepared.</strong></p>
+		</div>
 	</header>
 </template>
 
 <script>
-    import TheLogo from './TheLogo.vue';
+	import TheLogo from './TheLogo.vue';
+	import WeatherInput from './WeatherInput.vue';
 
 	export default {
         components: {
-            TheLogo,
+			TheLogo,
+			WeatherInput
         }
     };
 </script>
 
 <style lang="scss" scoped>
-	
+	h1 {
+		font-size: 48px;
+		margin-bottom: 0;
+	}
+
+	header {
+		margin-bottom: 48px;
+	}
+
+	p {
+		font-size: 48px;
+    	line-height: 1.2;
+		margin-top: 24px;
+	}
 </style>
