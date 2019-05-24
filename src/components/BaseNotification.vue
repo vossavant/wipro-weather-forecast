49b7e4ab7469ec36c2d3a75c08a1bf7e86b8b614
Ryan Burney <hello@ryanburney.com>
2019-05-24 15:40:28 -0600

    Create BaseNotification.vue

---
+++ src/components/BaseNotification.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="notification">
+        <h2 v-if="header">{{ header }}</h2>
+        <p>{{ content }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        header: {
+            type: String,
+            required: false
+        },
+        content: {
+            type: String,
+            required: true
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+    .notification {
+        background: white;
+        border-radius: 8px;
+        box-shadow: 2px 2px 8px transparentize(black, 0.9);
+        padding: 48px;
+
+        h2 {
+            margin-top: 0;
+        }
+    }
+</style>
